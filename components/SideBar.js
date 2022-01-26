fd0353244b6622ca62cc3a95db677b11b37c6c7f
nphenley <nathenley@gmail.com>
2022-01-26 18:38:14 +0000

    toggle sidebar

--- components/SideBar.js
+++ components/SideBar.js
@@ -1,15 +1,36 @@
-import { FaIdCard, FaNetworkWired, FaBriefcase, FaUser  } from "react-icons/fa";
-
+import { FaIdCard, FaNetworkWired, FaBriefcase, FaUser, FaBars, FaArrowLeft  } from "react-icons/fa";
+import {useState} from 'react'
 const SideBar = () => {
+
+    const [isOpen, setIsOpen] = useState(false)
+
     return (
-        <div className="fixed top-0 left-0 flex flex-col h-screen m-0 text-center bg-gray-900 shadow-lg w-1/10 text-cyan-400">
+        <>
+        {!isOpen ? 
+            (
+            <button onClick={()=>setIsOpen(!isOpen)} className="fixed top-0 right-0 pr-5">
+                    
+                    <FaBars  size="20"/>
+
+            </button>
             
-            <header className="mt-4">
-                Neo Tokyo Talent
-            </header>
-            <div className="flex-grow mx-2 mt-12 mb-20">
-             
-                
+            
+            
+            ):
+            (
+                        <button className="fixed z-10 top-6 left-16" onClick={()=>setIsOpen(!isOpen)}>
+                        <FaArrowLeft size="20" color="cyan"/>
+                        </button>
+               
+            )
+        }
+        <div className={`fixed top-0 left-0 flex flex-col h-screen m-0 text-center bg-gray-900 shadow-lg w-1/10 text-cyan-400 ${isOpen ? 'translate-x-0': '-translate-x-full'} ease-in-out duration-300`}>
+            
+            
+            <div className="flex-grow mx-2 mt-16 mb-20">
+
+                    
+
                     <SideBarIcon icon={<FaBriefcase size="20" />} text={"JOBS"}/>
                     <SideBarIcon icon={<FaIdCard size="20" />} text={"TALENT"}/>
                     <SideBarIcon icon={<FaNetworkWired size="20" />} text={"CONNECTIONS"}/>
@@ -19,8 +40,9 @@ const SideBar = () => {
             
 
             <ProfileIcon icon={<FaUser size="40" />}/>
-            
-        </div>
+
+</div>
+        </>
     )
 };
 
