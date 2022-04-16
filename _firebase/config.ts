c6bf550c8ef8339b7a220c7aa464e53c2db7bd02
Zachary Pepin <zachary.e.pepin@gmail.com>
2022-04-16 07:11:47 +0100

    Firebase functions fucking suck so just hooking Firebase up directly. Fuck JavaScript.

--- _firebase/config.ts
+++ _firebase/config.ts
@@ -1,15 +1,27 @@
 import { getFirestore } from 'firebase/firestore';
 import { initializeApp } from 'firebase/app';
 
-const firebaseConfig = {
+const app = initializeApp({
   apiKey: 'AIzaSyCmBMQy2AE153coEJqSeLO91fS6n0EZWS0',
   authDomain: 'talent-d15b4.firebaseapp.com',
   projectId: 'talent-d15b4',
   storageBucket: 'talent-d15b4.appspot.com',
   messagingSenderId: '691000709770',
   appId: '1:691000709770:web:71617ce4d81358cdd35373',
-};
-
-const app = initializeApp(firebaseConfig);
+});
 
 export const firestore = getFirestore(app);
+
+const collectionsApp = initializeApp(
+  {
+    apiKey: 'AIzaSyCYKhwCD01OtcfD7Tjx7YOJ8a7_ofNjPK0',
+    authDomain: 'staked-api.firebaseapp.com',
+    projectId: 'staked-api',
+    storageBucket: 'staked-api.appspot.com',
+    messagingSenderId: '629479392784',
+    appId: '1:629479392784:web:b6bc00432e9d5d9dc62c95',
+  },
+  'collections'
+);
+
+export const collectionsFirestore = getFirestore(collectionsApp);
--- _helpers/getNFTImages.ts
+++ _helpers/getNFTImages.ts
@@ -1,5 +1,7 @@
+import { doc, getDoc } from 'firebase/firestore';
 import { getEthCommunityIdsForCommunity, getSolCommunityIdsForCommunity } from '_api/communities';
 import { getAddressesInWalletGroup } from '_api/walletGroups';
+import { collectionsFirestore } from '_firebase/config';
 
 export const getCommunityNFTImagesForWalletGroup = async (
   walletGroupID: string,
@@ -7,23 +9,44 @@ export const getCommunityNFTImagesForWalletGroup = async (
   setNFTImages: any
 ) => {
   let walletAddresses;
-  let ethCommunityIds;
-  let solCommunityIds;
+  let ethCollectionIds;
+  let solCollectionIds;
   await Promise.all([
     (walletAddresses = await getAddressesInWalletGroup(walletGroupID)),
-    (ethCommunityIds = await getEthCommunityIdsForCommunity(communityId)),
-    (solCommunityIds = await getSolCommunityIdsForCommunity(communityId)),
+    (ethCollectionIds = await getEthCommunityIdsForCommunity(communityId)),
+    (solCollectionIds = await getSolCommunityIdsForCommunity(communityId)),
   ]);
 
-  const images = await (
-    await fetch('/api/images/', {
-      method: 'POST',
-      body: JSON.stringify({
-        walletAddresses: walletAddresses,
-        ethCommunityIds: ethCommunityIds,
-        solCommunityIds: solCommunityIds,
-      }),
+  const nfts:
+    | { tokenId: string; owner: string; staked: boolean; image: string }[]
+    | { tokenAddress: string; owner: string; staked: boolean; image: string }[] = [];
+
+  const fuckJavaScript: { walletAddress: string; collectionId: string; collectionType: 'sol' | 'eth' }[] = [];
+
+  for (const walletAddress of walletAddresses) {
+    for (const collectionId of ethCollectionIds)
+      fuckJavaScript.push({ walletAddress: walletAddress, collectionId: collectionId, collectionType: 'eth' });
+    for (const collectionId of solCollectionIds)
+      fuckJavaScript.push({ walletAddress: walletAddress, collectionId: collectionId, collectionType: 'sol' });
+  }
+
+  await Promise.all(
+    fuckJavaScript.map(async (trashLanguage) => {
+      const collectionPathName = trashLanguage.collectionType === 'eth' ? 'ethCollections' : 'collections';
+      const imagesPathName = trashLanguage.collectionType === 'eth' ? 'ethImages' : 'solImages';
+      const collectionRef = await getDoc(
+        doc(collectionsFirestore, `${collectionPathName}/${trashLanguage.collectionId}`)
+      );
+      if (!collectionRef.exists()) return;
+      const zeroIndexed = collectionRef.data()!.zeroIndexed;
+      const tokens = collectionRef.data()!.tokens;
+      const imagesRef = await getDoc(doc(collectionsFirestore, `${imagesPathName}/${trashLanguage.collectionId}`));
+      const images = imagesRef.exists() && imagesRef.data() ? imagesRef.data()!.images : [];
+      for (let i = zeroIndexed ? 0 : 1; i < tokens.length; i++)
+        if (tokens[i].owner === trashLanguage.walletAddress)
+          nfts.push({ ...tokens[i], image: images[i] ? images[i] : 'N/A' });
     })
-  ).json();
-  setNFTImages(images);
+  );
+
+  setNFTImages(nfts.map((token) => token.image));
 };
