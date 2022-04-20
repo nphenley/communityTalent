fab1e2d7ab51432814b7472f3cb0e13dde3059e5
Zachary Pepin <zachary.e.pepin@gmail.com>
2022-04-20 01:06:28 +0100

    Filled in founder profile for richpepsi

--- _constants/founderProfiles.ts
+++ _constants/founderProfiles.ts
@@ -15,13 +15,13 @@ export const somProfile: Profile = {
 
 export const richProfile: Profile = {
   displayName: 'richpepsi',
-  bio: 'Software Engineer, Web 3 Enthusiast',
+  bio: 'Software Engineer - Web Dev Warlord\nTechnology Lead for The Chimpions on Solana',
   dateCreated: Timestamp.now(),
   dateLastUpdated: Timestamp.now(),
   discordUsername: 'richpepsi#0335',
   twitterHandle: '@richpepsi_',
-  skills: ['Full-Stack Web Dev'],
   id: 'richpepsi',
   lookingForProject: false,
+  tags: ['Full-Stack Dev', 'Blockchain Dev', 'UI/UX Design'],
   profilePicture: 'assets/richpepsi.gif',
 };
